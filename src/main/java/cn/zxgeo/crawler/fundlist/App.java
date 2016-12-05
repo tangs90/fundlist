5d96af8ebe7ee72f9fc43a656a8f7b11927baf59
tangs90 <tangs90@qq.com>
2016-12-05 15:13:42 +0800

    first commit from y430P

---
+++ src/main/java/cn/zxgeo/crawler/fundlist/App.java
@@ -0,0 +1,13 @@
+package cn.zxgeo.crawler.fundlist;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
